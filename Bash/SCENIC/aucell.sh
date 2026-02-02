11396fbc6ac6270452e07cd323d6ee0dac95a9a7
ShankarD112 <143721592+ShankarD112@users.noreply.github.com>
2026-02-02 13:18:48 -0500

    Create aucell.sh

---
+++ Bash/SCENIC/aucell.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$ -o /path/pyscenic_aucell.out
+#$ -j y
+#$ -pe omp 16
+#$ -l h_rt=48:00:00
+#$ -P selneuro
+#$ -N pyscenic_aucell
+
+echo "JOB STARTED on $(hostname) at $(date)"
+
+SIF=/path/Singularity/aertslab-pyscenic-scanpy-0.12.1-1.9.1.sif
+WORKDIR=/path/
+
+cd "$WORKDIR"
+
+singularity exec \
+  --bind /path \
+  --bind "$PWD":"$PWD" \
+  "$SIF" \
+  pyscenic aucell \
+    sample_scenic_input.loom \
+    sample_reg.csv \
+    --output sample_SCENIC_output.loom \
+    --num_workers 16
+
+echo "JOB FINISHED at $(date)"
