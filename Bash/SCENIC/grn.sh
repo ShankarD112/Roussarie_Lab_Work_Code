e0ccd69e7308483fe9b0e965efefe35cc7440f5e
ShankarD112 <143721592+ShankarD112@users.noreply.github.com>
2026-02-02 13:21:25 -0500

    Create grn.sh

---
+++ Bash/SCENIC/grn.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#$ -o /path/pyscenic_grn.out
+#$ -j y
+#$ -pe omp 16
+#$ -l h_rt=48:00:00
+#$ -P selneuro
+#$ -N pyscenic_grn
+
+echo "JOB STARTED on $(hostname) at $(date)"
+
+# Path to Singularity image
+SIF=/path/Singularity/aertslab-pyscenic-scanpy-0.12.1-1.9.1.sif
+
+# Working directory with input/output files
+WORKDIR=/path
+cd $WORKDIR
+
+# Run pySCENIC inside Singularity
+singularity exec \
+  --bind /path \
+  --bind $PWD:$PWD \
+  $SIF \
+  pyscenic grn \
+    sample_scenic_input.loom \
+    feather_cistarget/allTFs_mm.txt \
+    -o sample_adj.csv \
+    --num_workers 16
+
+echo "JOB FINISHED at $(date)"
