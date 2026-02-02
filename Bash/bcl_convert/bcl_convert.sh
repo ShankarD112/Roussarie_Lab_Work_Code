3663b31c737119f9e5e2c112d336950f1cc7cf0b
ShankarD112 <143721592+ShankarD112@users.noreply.github.com>
2026-02-02 13:07:22 -0500

    Create bcl_convert.sh
    Added an example code to use bcl convert

---
+++ Bash/bcl_convert/bcl_convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#$ -o bcl_convert
+#$ -j y
+#$ -pe omp 16
+#$ -l h_rt=24:00:00
+#$ -P selneuro
+
+module load bcl-convert
+
+
+bcl-convert   --bcl-input-directory /home/path/   --output-directory  /home/path/   --no-lane-splitting  true  --no-sample-sheet true --force
