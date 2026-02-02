7a246e4e24c83394aaf97e1aef5019fc0403d8be
ShankarD112 <143721592+ShankarD112@users.noreply.github.com>
2026-02-02 13:26:05 -0500

    Create anysample_cogent_rna_analyze.sh

---
+++ Bash/Patch_seq/anysample_cogent_rna_analyze.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#$ -N combined_patch
+#$ -o path/analyze.log
+#$ -j y
+#$ -pe omp 28
+#$ -l mem_per_core=8G
+#$ -l h_rt=24:00:00
+#$ -P selneuro
+
+module load R/4.4.0      
+
+echo "Starting CogentAP analysis"
+mkdir -p "path"
+
+cogent rna analyze \
+    -g mm39 \
+    -i "path" \
+    -o "path" \
+    -G "path" \
+    -t smartseq_fla_umi
+
+echo "Finished"
